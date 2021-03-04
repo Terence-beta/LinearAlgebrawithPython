0cdf90403b435dd6442c400837e65bd99c474db5
Terence <terence112358@hotmail.com>
2021-03-04 21:54:24 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+import numpy
+import scipy
+
+if __name__ == '__main__':
+    print(numpy.__version__)
+    print(scipy.__version__)
+    print(sys.version)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
